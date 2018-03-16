16817270d3e8a863b4e1b6d48b72ac657ecf463d
Josh Han <jhan@adacado.com>
2018-03-16 15:14:33 -0700

    add offline check & add charged since when

--- fe/src/App.js
+++ fe/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
 
   constructor() {
     super();
-    let stations = [{"id":"20489a6b-ac43-4025-ac8a-0dd5ccf77f40","name":"AAA-10311","status":0,"last_updated":"2018-01-18T23:34:37.000Z"},{"id":"89dd06a3-b54c-4d38-9f19-7566ced6a791","name":"AAA-10322","status":0,"last_updated":"2018-01-18T23:34:37.000Z"}];
+    let stations = [{"id":"20489a6b-ac43-4025-ac8a-0dd5ccf77f40","name":"AAA-10311","status":0,"last_updated":"2018-03-16T21:29:47.000Z","last_status_changed":"2018-03-16T21:11:47.000Z"},{"id":"89dd06a3-b54c-4d38-9f19-7566ced6a791","name":"AAA-10322","status":1,"last_updated":"2018-03-16T21:29:47.000Z","last_status_changed":"2018-03-16T20:28:39.000Z"}];
     stations = [];
     this.state = {
       stations: stations
@@ -34,11 +34,19 @@ class App extends Component {
   }
 
   render() {
+    // 5 mins no updates, treat offline
+    const { stations } = this.state;
+    const last_updated = stations[0] && stations[0].last_updated;
+    const last_updated_ts = new Date(last_updated);    
+    const is_offline = (new Date() - last_updated_ts) > 5 * 60 * 1000;
+    const title = is_offline ? "Service Offline" : "Welcome to Flo Monitor";
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Flo Monitor</h1>
+        <header className={"App-header " + (is_offline ? "offline" : "") }>
+          { !is_offline &&
+            <img src={logo} className="App-logo" alt="logo" />
+          }
+          <h1 className="App-title">{title}</h1>
         </header>
         <div className="container mb-5">
           <div className="row">
--- fe/src/Station.js
+++ fe/src/Station.js
@@ -5,16 +5,27 @@ import bike from './imgs/bike.jpg';
 import * as ta from 'time-ago';
 
 class Station extends Component {
+
+    convert_to_h_m(time) {
+        const hh = Math.floor(time / 1000 / 60 / 60);
+        time -= hh * 1000 * 60 * 60;
+        const mm = Math.floor(time / 1000 / 60);
+        return `${hh} hours ${mm} minutes`;
+    }
+
     render() {
-        const {id, name, status, last_updated} = this.props.station;
-        const ts = new Date(last_updated);
+        const {id, name, status, last_updated, last_status_changed} = this.props.station;
+        const last_updated_ts = new Date(last_updated);
+        const charing_time = last_updated_ts - new Date(last_status_changed);
         return (
             <div className="col-sm-6">
                 <div className="card  mt-5">
                     <img className="card-img-top" src={status ? tesla : bike} alt="card image cap"/>
                     <div className={"card-body " + (status ? "bg-success" : "bg-danger")}>
                         <h1>{name}</h1>
-                        <p>Updated {ta.ago(ts)}</p>
+                        { !status && 
+                            <p>Charged for {this.convert_to_h_m(charing_time)}.</p>
+                        }
                     </div>
                 </div>
             </div>            
