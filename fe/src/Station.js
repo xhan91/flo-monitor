84a0176735029741e0eef2de48c9524075d99e04
Josh Han <jhan@adacado.com>
2018-03-16 15:41:06 -0700

    change pic of charging status

--- fe/src/Station.js
+++ fe/src/Station.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './Station.css';
 import tesla from './imgs/tesla.jpg';
-import bike from './imgs/bike.jpg';
+import teslaCharging from './imgs/tesla-charging.jpg';
 import * as ta from 'time-ago';
 
 class Station extends Component {
@@ -20,7 +20,7 @@ class Station extends Component {
         return (
             <div className="col-sm-6">
                 <div className="card  mt-5">
-                    <img className="card-img-top" src={status ? tesla : bike} alt="card image cap"/>
+                    <img className="card-img-top" src={status ? tesla : teslaCharging} alt="card image cap"/>
                     <div className={"card-body " + (status ? "bg-success" : "bg-danger")}>
                         <h1>{name}</h1>
                         { !status && 
